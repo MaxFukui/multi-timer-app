82d4dd7063e0690d6c87fd508dde14c589301b35
Max Fukui <max.fukui.dev@gmail.com>
2024-01-24 19:27:56 -0400

    modified:   package-lock.json 	modified:   package.json // Adding new packages, react-dom and 	modified:   src/App.tsx // Fixing Routes 	modified:   src/index.tsx // Adding Routes features
     Untracked files:
    	src/store/  // Redux settings

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import TimerPage from "./pages/TimesPage";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <TimerPage />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
+
+//Please, take away the React.StrictMode tag from the index.tsx file,
+//when you are ready to deploy your app.
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
